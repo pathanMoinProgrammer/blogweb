eac2b4d5f767c4ed38957e954676c6ebbd044bc9
pathanMoinProgrammer <ramjankhanpathan20@gmail.com>
2025-10-19 16:14:33 +0530

    done

---
+++ v.js
@@ -0,0 +1,45 @@
+// const array = [1, 4, 3, 3, 1, 4, 5, 6];
+// let newArray = [];
+
+// for (let item of array) {
+//   if (!newArray.includes(item)) {
+//     newArray.push(item);
+//   }
+// }
+
+// for (let i = 1; i < newArray.length; i++) {
+//   if (newArray[i] < newArray[i - 1]) {
+//     let temp = newArray[i];
+//     newArray[i] = newArray[i - 1];
+//     newArray[i - 1] = temp;
+//   }
+// }
+
+// console.log(newArray);
+
+
+
+
+
+// 'use client';
+// import React, { useState } from 'react';
+
+// const page = () => {
+//   const [state, seState] = useState(2);
+//   return (
+//     <div>
+//       <ACom state={state} />
+//       <BCom setData={seState} />
+//     </div>
+//   );
+// };
+
+// const ACom = ({ state }) => {
+//   return <div>{state} aa</div>;
+// };
+
+// const BCom = ({ setData }) => {
+//   return <button onClick={() => setData((prev) => prev + 1)}>Click</button>;
+// };
+
+// export default page;
